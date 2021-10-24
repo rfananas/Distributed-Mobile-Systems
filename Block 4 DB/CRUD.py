0f6e197ecccb0eb00a3b4513662477553402a655
rfananas <rfananas89@gmail.com>
2021-10-24 15:45:25 +0200

    Nachbearbeitungen

---
+++ Block 4 DB/CRUD.py
@@ -0,0 +1,109 @@
+
+from waltisLibrary import *
+import mysql.connector
+
+
+print("Connecting to DB....", end="", flush=True)
+mydb = mysql.connector.connect(
+  host     = "localhost",
+  user     = "root",
+  passwd   = "admin",
+  auth_plugin = 'mysql_native_password'
+)
+print("completed!\n\n")
+
+# DDL: Create schema
+print("Create schema....", end="", flush=True)
+mycursor = mydb.cursor()
+mycursor.execute("DROP SCHEMA IF EXISTS DelMe")
+mycursor.execute("CREATE DATABASE DelMe")
+mycursor.execute("USE DelMe")
+print("completed!\n\n")
+
+# DDL: Check available schemas
+print("Show schemas....", end="", flush=True)
+mycursor.execute("SHOW DATABASES")
+for aSchema in mycursor:
+  print(aSchema)
+print("completed!\n\n")
+
+# DDL: Create table
+print("Create table....", end="", flush=True)
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+print("completed!\n\n")
+
+# DDL: Check available tables
+print("Show tables....", end="", flush=True)
+mycursor.execute("SHOW TABLES")
+for aTable in mycursor:
+  print(aTable)
+print("completed!\n\n")
+
+# Insert data into table
+print("Inserts data....")
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+print("A single record inserted ", val, " , ID:", mycursor.lastrowid)
+print(mycursor.rowcount, "record inserted.")
+mydb.commit()
+print(mycursor.rowcount, "record inserted.\n")
+
+# Update data from table
+print("Update data....")
+updateSQL = f"  UPDATE customers SET name='Walti' WHERE id=1"
+print(updateSQL, "\n\n")
+mycursor.execute(updateSQL)
+halt("Update commit?")
+mydb.commit()
+halt("After commit?")
+
+# Delete data from table
+print("Delete data....")
+deleteSQL = f"  Delete from customers WHERE id=1"
+print(deleteSQL, "\n\n")
+mycursor.execute(deleteSQL)
+halt("Delete commit?")
+mydb.commit()
+halt("After commit?")
+
+print("Bulk load ... insert")
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+mycursor.executemany(sql, val)
+print(mycursor.rowcount, "record inserted.")
+halt("Bulkload commit?")
+mydb.commit()
+halt("After bulkload commit!")
+print(mycursor.rowcount, "record inserted.")
+print("completed!\n\n")
+
+# Check data
+print("Show data...\n", end="", flush=True)
+print(mycursor.rowcount, "record inserted.")
+stm_selectCities = """
+    SELECT
+       * 
+    FROM 
+       customers
+"""
+mycursor.execute(stm_selectCities)
+myresult = mycursor.fetchall()
+print("Records found:", len(myresult), myresult)
+
+print("completed!\n\n")
---
+++ Block 4 DB/Call_StoredProcedure_Sakila.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------
+# Name: 05_Call_StoredProcedure_Sakila.py
+#
+# Description: Connects to sakila and calls stored procedures
+# https://www.mysqltutorial.org/calling-mysql-stored-procedures-python/
+#
+#
+# Autor: Walter Rothlin
+#
+# History:
+# 18-Oct-2021   Walter Rothlin      Initial Version
+# ------------------------------------------------------------------
+import mysql.connector as mc # mysql-connector-python not default mässiger one
+import sys
+
+'''
+-- Create_Add_StoredProcedure.sql
+-- ------------------------------
+
+DELIMITER //
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE add_num(IN num1 INT, IN num2 INT, OUT sum INT)
+BEGIN
+SET sum := num1 + num2;
+END //
+
+DELIMITER ;
+
+'''
+# =============================
+# MAIN
+# =============================
+dbServer = "localhost"
+dbSchema = "sakila"
+userName = "root"
+password = "admin"
+
+try:
+    print(f"Connecting to '{dbSchema:s}' with user '{userName:s}'....", end="", flush=True)
+    # https://dev.mysql.com/doc/connector-python/en/connector-python-connectargs.html
+    conn = mc.connect(
+        host=dbServer,
+        database=dbSchema,
+        user=userName,
+        passwd=password
+    )
+    print("completed!")
+
+    myCursor = conn.cursor()
+    args = (7, 6, 0) # 0 is to hold value of the OUT parameter sum
+    result_args = myCursor.callproc('add_num', args) #callproc = die stored procedure aufrufen
+    print("add_num:", args[0], "+", args[1], "=", result_args[2])
+
+except mc.Error as e:
+    print("\nError {errNo:d}: {errTxt:s}".format(errNo=e.args[0], errTxt=e.args[1]))
+    sys.exit(1)
+
+finally:
+        myCursor.close()
+        conn.close()
